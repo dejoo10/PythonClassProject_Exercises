6147271ed5071dadf827ff7858a509922a7e43f0
dejoo10 <daygme@gmail.com>
2022-08-25 16:13:41 +0300

    assignment 1-5

---
+++ main.py
@@ -0,0 +1,77 @@
+import sys
+
+print("This is a program that make use of the medieval units")
+Talent = float(input("enter the value of talent:"))
+Pounds = float( Talent*20)
+lot = float( Pounds*13.3)
+
+
+print("the weight in grams (g) is =", lot ,  "and the weight in Kilogram (Kg) is =", lot/1000)
+
+
+sys.exit(6)
+
+################################
+
+print("This is a program that make use of the medieval units")
+Talent = float(input("enter the value of talent:"))
+Pounds = float(input("enter the value of pounds:"))
+lot = float(input("enter the value of lot:"))
+print("the value in Kg is =", Talent*20, "Kg")
+print("the value in pounds is =", Pounds*32, "pound" )
+print("the value in grams is =", lot*13.3, "grams")
+
+
+sys.exit(4)
+
+
+###########################################
+
+inte1= int (input ("enter integer 1 ="))
+inte2= int (input ("enter integer 2 ="))
+inte3= int (input ("enter integer 3 ="))
+
+print("the sum of your selection is =", inte1+inte3+inte2)
+print("the product of your selection is =", inte1*inte3*inte2)
+print("the average of your selection is =", (inte3+inte2+inte1)/3)
+
+
+
+sys.exit(3)
+
+#########################################################
+
+lenght= float(input("enter value of lenght:"))
+width= float(input("enter value of widht:"))
+print("the Area of the rectangle is =", lenght*width )
+print("The perimeter of the triangle is =", lenght+width+lenght+width)
+
+
+
+
+
+sys.exit(2)
+
+###############################################################
+
+#This is the Second exercise to be presented 26.08.2022
+
+User = input("Give name:")
+print("Hello :" + User +"!")
+r =float(input("enter radius value in meters :"))
+print("do you know the radius (r) of a circle?")
+
+
+print ("area of a circle is =" , 3.142*(r*r))
+###########################################################
+
+sys.exit(1)
+#name = input("give name")
+#print (name)
+
+#sys.exit(0)
+
+
+
+#print ("Hello, World!")
+#print ('"hello"')
