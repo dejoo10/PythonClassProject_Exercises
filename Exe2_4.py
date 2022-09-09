bc2621df316dbf6d4d9f6371c9d34b0a1911ef5f
dejoo10 <daygme@gmail.com>
2022-09-09 10:23:08 +0300

    renamed all exercise 2 to fit the number

---
+++ Exe2_4.py
@@ -0,0 +1,18 @@
+import sys
+
+
+#Here is exercise 2_4
+
+import sys
+
+inte1= int (input ("enter integer 1 ="))
+inte2= int (input ("enter integer 2 ="))
+inte3= int (input ("enter integer 3 ="))
+
+print("the sum of your selection is =", inte1+inte3+inte2)
+print("the product of your selection is =", inte1*inte3*inte2)
+print("the average of your selection is =", (inte3+inte2+inte1)/3)
+
+
+
+sys.exit(0)
