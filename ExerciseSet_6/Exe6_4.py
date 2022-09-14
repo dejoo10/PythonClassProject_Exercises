738ba4e7469e4e494496f239db78e6ccb2344771
dejoo10 <daygme@gmail.com>
2022-09-14 13:58:13 +0300

    Exercise 6_4 is done

--- ExerciseSet_6/Exe6_4.py
+++ ExerciseSet_6/Exe6_4.py
@@ -1,11 +1,14 @@
 import sys
 
+
+
 def list_int(values):
+    Sum = sum(values)
+    print(Sum)
+
+list1 = [2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+(list_int(list1))
 
 
-
-
-list1 = ["2", "5", "20", "5", "10"]
-
-print(list_int(list1))
-
+sys.exit(0)
