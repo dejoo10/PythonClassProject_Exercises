e74cc710e874c5d417b7e34e501215cab8d19c55
dejoo10 <daygme@gmail.com>
2022-09-14 13:58:26 +0300

    Exercise 6_5 is done

--- ExerciseSet_6/Exe6_5.py
+++ ExerciseSet_6/Exe6_5.py
@@ -1,14 +1,16 @@
 import sys
 
 def con_value(slave):
-    print(slave)
+    for num in slave:
+        if num % 2 == 0:
+            list_2.append(num)
+    print("Here is the original list", slave)
+    print("Here is the list without uneven numners", list_2)
 
 
+list_1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+list_2 = []
 
-
-
-
-list_1 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
 con_value(list_1)
 
 
