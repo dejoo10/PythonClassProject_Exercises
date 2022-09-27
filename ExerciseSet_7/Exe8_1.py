59d0da83047a63c26dac69f57f05838910e68082
dejoo10 <daygme@gmail.com>
2022-09-27 23:00:45 +0300

    completed Execise 8_1

---
+++ ExerciseSet_7/Exe8_1.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+
+conn = mysql.connector.connect( host= "localhost",
+                                user = "root",
+                                passwd = "Ambidextrous",
+                                database= "flight_game"
+                                )
+
+
+user = input("Enter the ICAO code of any airport:")
+config = conn.cursor()
+config.execute("select name, location from airport, game where ident = " + "'" + user + "'")
+
+for i in config:
+    print(i)
+
+
+#print(config)
+
+
+
+
