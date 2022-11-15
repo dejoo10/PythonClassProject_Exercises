6c265f869a1bc6b02830ed3d2ba030674c07a5b3
dejoo10 <daygme@gmail.com>
2022-11-15 12:26:18 +0200

    Exercise set 10_3 complete

---
+++ ExerciseSet__10/Exe10_3.py
@@ -0,0 +1,54 @@
+import sys
+
+
+class Elevator:
+    def __init__(self, floors, current_floor = 0):
+        self.floors = floors
+        self.current_floor = current_floor
+
+    def floor_up(self):
+        self.current_floor += 1
+        print(f'Going up, current floor is {self.current_floor}')
+
+    def floor_down(self):
+        self.current_floor -= 1
+        print(f'Going down, current floor is {self.current_floor}')
+
+    def go_to_floor(self, selected_floor):
+        if self.current_floor < selected_floor and selected_floor <= self.floors:
+            for i in range(selected_floor):
+                self.floor_up()
+
+        elif self.current_floor > selected_floor and self.current_floor - selected_floor > 0:
+            for i in range(self.current_floor - selected_floor):
+                self.floor_down()
+
+
+class Building:
+    def __init__(self, floors, elevator):
+        self.floors = floors
+        self.elevator = elevator
+
+    @property
+    def elevators(self):
+        elevs = []
+        for i in range(self.elevator):
+            elevs.append(Elevator(self.floors))
+        return elevs
+    def run_elevator(self, floor, elevators):
+        for elevator in self.elevators[:elevators]:
+            elevator.go_to_floor(floor)
+#fire alarm part
+    def fire_alarm(self):
+        for elevator in self.elevators:
+            elevator.current_floor = 0
+
+#Main part
+
+build = Building(10, 5)
+#print(build.floors)
+#print(build.elevators)
+build.run_elevator(3, 2)
+build.fire_alarm()
+
+sys.exit(0)
