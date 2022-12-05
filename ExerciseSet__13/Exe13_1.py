b5ce82621eb01194a88123a8cdfad8306a428de5
dejoo10 <daygme@gmail.com>
2022-12-06 00:35:13 +0200

    Exercise set 13_1

--- ExerciseSet__13/Exe13_1.py
+++ ExerciseSet__13/Exe13_1.py
@@ -1,37 +1,34 @@
 import sys
-from math import sqrt
 import json
 from flask import Flask, request, Response
 
+#The code looks ok, but it is not responding on the web
 
 app = Flask(__name__)
 @app.route('/prime')
 def prime():
     args = request.args
-    number = float(args.get("number"))
-    prime = prime_ber(number)
+    num = int(args.get("number"))
+
+    prime_check = 0
+
+    if num > 1:
+        for i in range(2, num):
+            if (num % i == 0):
+                prime_check = "True"
+                break
+    if prime_check:
+        prime_check = "False"
+    else:
+        prime_check = "True"
+
+    aaa = prime_check
 
     response = {
-        "Number" : number,
-        "isPrime" : prime
+        "Number" : num,
+        "isPrime" : aaa
     }
     return response
-
-def prime_ber(num):
-    prime_check = 0
-    if(num>1):
-        for i in range(2, int(sqrt(num)) + 1):
-            if (num % i == 0):
-                prime_check = 1
-                break
-        if (prime_check == 0):
-            prime("True")
-        else:
-            print("False")
-    else:
-        print("False")
-    return prime_ber
-
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=5000)
